69674a42a20fa8fd4d7c0c6e245bfeeaea3af2df
Pragyagit-07 <pragyamakadariya7@gmail.com>
2026-01-22 18:34:10 +0530

    add new api

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -43,7 +43,7 @@ export const searchMovies = async (req, res) => {
   try {
     const q = req.query.query || "";
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 16;
+    const limit = Number(req.query.limit) || 200;
     const skip = (page - 1) * limit;
 
     const filter = {
